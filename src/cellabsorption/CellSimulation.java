7ca30c4e827638d58254aac87a1078ded8de131e
karynasteele <ksteele1@macalester.edu>
2024-10-03 12:48:47 -0500

    Complete

--- src/cellabsorption/CellSimulation.java
+++ src/cellabsorption/CellSimulation.java
@@ -29,7 +29,7 @@ public class CellSimulation {
             Point canvasCenter = new Point(canvas.getWidth() / 2.0, canvas.getHeight() / 2.0);
             for(Cell cell : cells) {
                 cell.moveAround(canvasCenter);
-                cell.grow(0.02);
+                handleCellInteraction();
             }
             canvas.draw();
             canvas.pause(10);
@@ -47,6 +47,17 @@ public class CellSimulation {
             Color.getHSBColor(rand.nextFloat(), rand.nextFloat() * 0.5f + 0.1f, 1));
             cells.add(cell);
             canvas.add(cell.getShape());
+        }
+    }
+
+    private void handleCellInteraction() {
+        // for i from 0 up to max cell index
+        for(int i = 0; i < cells.size(); i++) {
+            Cell cell0 = cells.get(i);
+            for(int j = i + 1; j < cells.size(); j++) {
+                Cell cell1 = cells.get(j);
+                cell0.interactWith(cell1);
+            }
         }
     }
 
